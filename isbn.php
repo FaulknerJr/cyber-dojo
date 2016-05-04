2aa627e9ebb8df7c1e4e27baef6aa08f389c709c
Mike Stratton <mike@mikestratton.net>
2016-05-03 22:46:17 -0400

    Update isbn.php

--- isbn.php
+++ isbn.php
@@ -1,9 +1,24 @@
 <?php
-	$str = "1 23 45  adf 67";
-	echo preg_match_all( "/[0-9]/", $str );
-	
-	
-	
 
-	
+// There are two ISBN standards: 
+// ISBN-10 and ISBN-13.
+
+// ISBN-10
+// ISBN-10 is made up of 9 digits plus a 
+// check digit (which may be 'X') 
+function tst01()
+{
+    $iTen = "0123456789x";
+    return preg_match_all( "/[0-9]/", $iTen );
+}
+
+// ISBN-13
+// ISBN-13 is made up of 12 digits plus a
+// check digit. 
+function tst02()
+{
+    $iThr = "0123456789123x";
+    return preg_match_all( "/[0-9]/", $iThr);
+}
+
 ?>
