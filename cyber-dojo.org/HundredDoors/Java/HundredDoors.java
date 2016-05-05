9efdbec05dd36add4d6f155076f558f001aafea9
FaulknerJr <Workstation@Jeffreys-iMac.home>
2016-05-04 21:01:37 -0400

    This code works better than previous push. Check it out and see if you can fix last error :)

--- cyber-dojo.org/HundredDoors/Java/HundredDoors.java
+++ cyber-dojo.org/HundredDoors/Java/HundredDoors.java
@@ -10,27 +10,33 @@
  */
 public class HundredDoors {
     public void run(){
+
         playingWithDoors();
     }
     
     public void playingWithDoors(){
-        int [] doors = new int [101];
+        int [] doors = new int[101];
         for(int i = 0; i < doors.length; ++i){
             doors[i] = 0;
         }
-        for(int i = 1; i < doors.length; ++i){
+        for(int i = 1; i < 101; ++i){
             int j = i;
-            while(j < 100){                
-                if(doors[j] == 0){
-                    doors[j] = 1;
-                } else {
-                    doors[j] = 0;
-                }            
-                j = ((j + 1) * i);    
+            while(j < 100){
+                if(j % i == 0) {
+                    if (doors[j] == 0) {
+                        doors[j] = 1;
+                    } else {
+                        doors[j] = 0;
+                    }
+                }
+                j = (j + i);
             }
         }
         int doorsOpen = 0;
         int doorsClosed = 0;
+
+        System.out.println("Opened doors are 1. Closed doors are 0");
+
         for(int i = 1; i < doors.length; ++i){
             if(doors[i] == 1){
                 ++doorsOpen;
--- cyber-dojo.org/HundredDoors/Java/README.md
+++ cyber-dojo.org/HundredDoors/Java/README.md
@@ -1,5 +1,6 @@
 If you want to run this project
 
+
 cd PATH/to/files
 
 $ javac HundredDoors.java
@@ -7,3 +8,8 @@ $ javac HundredDoors.java
 $ javac main.java 
 
 $ java main 
+
+Addendum:
+The code works semi properly right now. There is an error where the final door
+	isn’t in the proper position. I am not sure how to fix it. Pushing
+	with hopes someone else may be able to figure it out.
